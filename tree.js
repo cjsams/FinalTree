016c9478e26a8089699952eea2cee8ba7b0e5f35
cjsams <csams@mail.bradley.edu>
2015-12-10 03:09:59 -0600

    does enter work now

--- tree.js
+++ tree.js
@@ -2,3 +2,12 @@ $('#content').click(function() {
     $('form').removeClass('hidden');
     $('#content').addClass('hidden');
 });
+
+$('#textareaid').bind('keypress', function(e) {
+        if(e.keyCode==13){
+                // Enter pressed... do anything here...
+                   $(this).val(  $(this).val() + "<br/>");
+
+
+        }
+});
