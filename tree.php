52f844e33cb7c07f062e343a7562f20e20721272
cjsams <csams@mail.bradley.edu>
2015-12-10 01:15:09 -0600

    tree not wiki

--- tree.php
+++ tree.php
@@ -3,8 +3,8 @@
     <head>
 
         <meta charset="utf-8">
-        <link rel="stylesheet" href="wiki.css">
-        <title>Wiki</title>
+        <link rel="stylesheet" href="tree.css">
+        <title>Tree</title>
     </head>
 
     <body>
@@ -13,11 +13,11 @@
 
       if (isset($_GET['content'])) {
         $content = $_GET['content'];
-        file_put_contents('wiki.txt', $content);
+        file_put_contents('tree.txt', $content);
     }
 
-    if (file_exists('wiki.txt')) {
-        $content = file_get_contents('wiki.txt');
+    if (file_exists('tree.txt')) {
+        $content = file_get_contents('tree.txt');
     } else {
         $content = '(no content)';
     }
@@ -26,7 +26,7 @@
     echo $safe_content;
     ?>
 
-  <form action="wiki.php" class="hidden">
+  <form action="tree.php" class="hidden">
 
     <textarea name="content" rows="8" cols="80"></textarea>
     <input type="submit" value="Save">
@@ -41,7 +41,7 @@
     <?php echo $safe_content; ?>
   </div>
 
-<script src="wiki.js"></script>
+<script src="tree.js"></script>
   </body>
 
 </html>
