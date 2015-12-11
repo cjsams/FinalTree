92364dcc68b713c2051c054491a5f8155c5e4eec
cjsams <csams@mail.bradley.edu>
2015-12-10 22:44:32 -0600

    nope

--- tree.js
+++ tree.js
@@ -4,18 +4,3 @@ document.getElementById("root").style.color = "brown";
 document.getElementById("leaf2").style.color = "green";
 document.getElementById("trunk2").style.color = "BurlyWood";
 document.getElementById("root2").style.color = "brown";
-
-
-jQuery( document ).ready(function()
-{
-
-    $('#leaf2').each(function() {
-        var h = $(this).html();
-        var index = h.indexOf(' ');
-        if(index == -1) {
-            index = h.length;
-        }
-        $(this).html('<span style="color:red;">' + h.substring(0, index) + '</span>' + h.substring(index, h.length));
-    });
-
-});
